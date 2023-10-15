a58bab211f587ba46038ef9cb6659d0c74dfaa39
roshan <roshan.shinde164@gmail.com>
2023-10-15 08:30:05 +0530

    added binding adapter - custom setters for custom properties of view

---
+++ app/src/main/java/com/example/marsphotos/BindingAdapters.kt
@@ -0,0 +1,14 @@
+package com.example.marsphotos
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import coil.load
+
+@BindingAdapter("imgUrl")   //tells data binding to execute this binding adapter when a View item has the imageUrl attribute.
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        imgUrl.toUri().buildUpon().scheme("https").build()
+        imgView.load(imgUrl)
+    }
+}
