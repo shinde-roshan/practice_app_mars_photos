4b58aba0b3d28b04b91e98e003c7e1b4965c317a
roshan <roshan.shinde164@gmail.com>
2023-10-22 12:17:38 +0530

    Error handling

--- app/src/main/java/com/example/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/example/marsphotos/BindingAdapters.kt
@@ -1,11 +1,13 @@
 package com.example.marsphotos
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.marsphotos.network.MarsPhoto
+import com.example.marsphotos.overview.MarsApiStatus
 import com.example.marsphotos.overview.PhotoGridAdapter
 
 @BindingAdapter("imgUrl")   //tells data binding to execute this binding adapter when a View item has the imageUrl attribute.
@@ -24,4 +26,22 @@ fun bindingRecyclerView(recyclerView: RecyclerView, data: List<MarsPhoto>?)
 {
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("apiStatus")
+fun bindingStatusImage(imageView: ImageView, status: MarsApiStatus)
+{
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            imageView.visibility = View.VISIBLE
+            imageView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            imageView.visibility = View.VISIBLE
+            imageView.setImageResource(R.drawable.ic_connecction_error)
+        }
+        MarsApiStatus.DONE -> {
+            imageView.visibility = View.GONE
+        }
+    }
 }
--- app/src/main/java/com/example/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/marsphotos/overview/OverviewViewModel.kt
@@ -8,10 +8,11 @@ import com.example.marsphotos.network.MarsApi
 import com.example.marsphotos.network.MarsPhoto
 import kotlinx.coroutines.launch
 
+enum class MarsApiStatus { LOADING, DONE, ERROR }
 class OverviewViewModel : ViewModel() {
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
     private var _photos = MutableLiveData<List<MarsPhoto>>()
-    val status: LiveData<String> = _status
+    val status: LiveData<MarsApiStatus> = _status
     val photos: LiveData<List<MarsPhoto>> = _photos
 
     init {
@@ -19,13 +20,15 @@ class OverviewViewModel : ViewModel() {
     }
 
     private fun getMarsPhotos() {
-        try {
-            viewModelScope.launch {
+        viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
+            try {
                 _photos.value = MarsApi.retrofitService.getPhotos()
-                _status.value = "Success: ${_photos.value!!.size} mars photos received."
+                _status.value = MarsApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
             }
-        } catch (e: Exception) {
-            _status.value = e.message
         }
     }
 }
